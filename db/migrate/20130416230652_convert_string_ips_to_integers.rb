a9d41097fafa86ea1fe791599699f0b2f23b8712
Evan Louie <mail@evanlouie.com>
2015-03-24 18:08:06 -0700

    ip migration: data retention complete

--- db/migrate/20130416230652_convert_string_ips_to_integers.rb
+++ db/migrate/20130416230652_convert_string_ips_to_integers.rb
@@ -4,14 +4,21 @@ class ConvertStringIpsToIntegers < ActiveRecord::Migration
     users = User.all
     change_column :users, :current_sign_in_ip, :integer
     change_column :users, :last_sign_in_ip,    :integer
-    users.each{ |u| u.save }
+    users.each do |u|
+      u.current_sign_in_ip= u[:current_sign_in_ip] if u[:current_sign_in_ip] != nil
+      u.last_sign_in_ip= u[:last_sign_in_ip] if u[:last_sign_in_ip] != nil
+      u.save
+    end
   end
 
   def down
     users = User.all
     change_column :users, :current_sign_in_ip, :string
     change_column :users, :last_sign_in_ip,    :string
-    users.each{ |u| u.save }
+    users.each do |u|
+      u.current_sign_in_ip= u[:current_sign_in_ip] if u[:current_sign_in_ip] != nil
+      u.last_sign_in_ip= u[:last_sign_in_ip] if u[:last_sign_in_ip] != nil
+      u.save
+    end
   end
-
 end
